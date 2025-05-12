9ffeb0612f20ddf9402866305ee71ef430728617
Blancaclaire <clara.alonso.jimenez@gmail.com>
2025-05-12 22:31:43 +0200

    Delete CrazyCow_Server directory

--- CrazyCow_Server/src/main/java/Exceptions/DuplicateIngredientException.java
+++
@@ -1,7 +0,0 @@
-package Exceptions;
-
-public class DuplicateIngredientException extends Exception{
-    public  DuplicateIngredientException(String message){
-        super(message);
-    }
-}
--- CrazyCow_Server/src/main/java/MotorMySql/IMotorSql.java
+++
@@ -1,9 +0,0 @@
-package MotorMySql;
-
-import java.sql.ResultSet;
-
-public interface IMotorSql {
-    public void connect();
-    public ResultSet executeQuery(String sql);
-    public void disconnect();
-}
